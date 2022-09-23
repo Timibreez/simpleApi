4a5526b3921f329b6907bf8ba233eb540cab5940
Timibreez <ffftimi@gmail.com>
2022-09-23 09:54:52 +0100

    HotelBookingController: setup controller.

---
+++ SimpleApi/Controllers/HotelBookingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+    }
+}
